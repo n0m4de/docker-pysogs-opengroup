9867966fbfdff44ecd30fbd947793350e1bd650b
n0m4de <76074876+n0m4de@users.noreply.github.com>
2023-07-29 14:36:24 +0200

    Update run.sh

--- pysogs/run.sh
+++ pysogs/run.sh
@@ -1,5 +1,11 @@
 apt -y install sogs-standalone
 python3 -msogs --initialize
+
+chmod a+rw key_x25519
+chmod a+rw sogs.db
+mv key_x25519 /var/lib/session-open-group-server/
+mv sogs.db /var/lib/session-open-group-server/
+
 systemctl restart sogs-standalone.service
-python3 -msogs --add-room fish --name "Fishing" --description "Australian fisheries chat"
-python3 -msogs --rooms + --add-moderators SESSIONID --admin --hidden
+
+sogs

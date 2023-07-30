80da50c9a1016b0a1a6528698f655e6308a0ef93
n0m4de <76074876+n0m4de@users.noreply.github.com>
2023-07-30 10:25:11 +0200

    Update run.sh

--- pysogs/run.sh
+++ pysogs/run.sh
@@ -1,11 +1,1 @@
-apt -y install sogs-standalone
-python3 -msogs --initialize
 
-chmod a+rw key_x25519
-chmod a+rw sogs.db
-mv key_x25519 /var/lib/session-open-group-server/
-mv sogs.db /var/lib/session-open-group-server/
-
-systemctl restart sogs-standalone.service
-
-sogs
